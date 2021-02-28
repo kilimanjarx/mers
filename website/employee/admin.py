326f2b94b5226f67ba0e2e489c9f3603fb6f247e
riccardo <riccardo.ginchang@gmail.com>
2021-02-28 22:04:39 +0800

    changes

--- website/employee/admin.py
+++ website/employee/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
+from import_export import resources
+from .resources import EmployeeResource
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+class EmployeeAdmin(ImportExportModelAdmin):
+    resource_class = EmployeeResource
+    list_display  = ['employee_id', 'name','division','section','position','grade','is_expatriate','is_exclude','join_date','resign_date','resign_flag','address']
+admin.site.register(Employee, EmployeeAdmin)
+
+# class LogAdmin(admin.ModelAdmin):
+#     list_display = ('message', 'date')
+# admin.site.register(Log, LogAdmin)
--- website/employee/views.py
+++ website/employee/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from employee.models import Employee
 from django.http import JsonResponse
@@ -7,17 +7,17 @@ from django.contrib.auth.models import User
 from django.views.generic.base import TemplateView
 from django.views import View
 from django.contrib import messages
+from django.urls import reverse
+from django.utils import timezone
 
 # Create your views here.
 class Index(View):
     template_name = "home.html"
 
     def get(self, request,*args, **kwargs):
-        
         emp = []
         emp = Employee.objects.filter(employee_id=request.user.username).values()
         list_result = [entry for entry in emp]
-        print(list_result)
         ctx = {
             'param': list_result,
         }
@@ -25,17 +25,11 @@ class Index(View):
         return render(request, self.template_name,ctx)
         # return JsonResponse(list_result,safe=False)
 
-    def post(self, request):
-        emp_id = request.POST.get('employee_id')
+    def post(self, request,*args, **kwargs):
         address = request.POST.get('address')
-        clerk_input_address = request.POST.get('clerk_input_address')
         try:
-            if clerk_input_address:
-                Employee.objects.filter(employee_id=emp_id).update(address=clerk_input_address)
-            else:
-                Employee.objects.filter(employee_id=request.user.username).update(address=address)
+            Employee.objects.filter(employee_id=request.user.username).update(address=address)
             messages.success(request, 'Form submission successful')
-
         except Exception as e:
             messages.error(request, e)
 
@@ -70,14 +64,35 @@ class Clerk_Update(View):
     def get(self, request,*args, **kwargs):
         return render(request, self.template_name)
 
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
+        emp_id = request.POST.get('user_id')
+        clerk_employee_id = request.POST.get('employee_id')
         emp = []
+
         user = Employee.objects.filter(employee_id=request.user.username).values()
-        emp = Employee.objects.filter(employee_id=request.POST.get('user_id')).values()
+        emp = Employee.objects.filter(employee_id=emp_id).values()
 
         user_list_result = [entry for entry in user]
         emp_list_result = [entry for entry in emp]
-
+        
+        
+        #  update employee by clerk
+        if request.POST.get('clerk_input_address'):
+            emp_id = request.POST.get('employee_id')
+            clerk_input_address = request.POST.get('clerk_input_address')
+            try:
+                Employee.objects.filter(employee_id=clerk_employee_id).update(address=clerk_input_address)
+                clerk_employee = Employee.objects.filter(employee_id=clerk_employee_id).values()
+                clerk_employee = [entry for entry in clerk_employee]
+                messages.success(request, 'successful submit')
+                ctx = {
+                'param': clerk_employee,
+                }
+                return render(request, self.template_name,ctx)
+            except Exception as e:
+                messages.error(request, e)
+            
+        # checking if clerk is allowed to update the employee
         if emp_list_result[0]['section'] != user_list_result[0]['section']:
             messages.error(request, 'Please input employee in your section or division only!')
             return render(request, self.template_name)
